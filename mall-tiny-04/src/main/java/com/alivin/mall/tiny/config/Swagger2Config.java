bb2e3567c2d94daa7dd7ef4a01860ecb45ceff55
alivinfer <942122009@qq.com>
2021-08-11 13:52:39 +0800

    SpringSecurity + JWT

--- mall-tiny-04/src/main/java/com/alivin/mall/tiny/config/Swagger2Config.java
+++ mall-tiny-04/src/main/java/com/alivin/mall/tiny/config/Swagger2Config.java
@@ -6,10 +6,13 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Swagger2API文档的配置
@@ -43,7 +46,41 @@ public class Swagger2Config {
                 //为当前包下controller生成API文档
                 .apis(RequestHandlerSelectors.basePackage("com.alivin.mall.tiny.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                // 添加登录认证
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
+    }
+
+    private List<SecurityScheme> securitySchemes() {
+        //设置请求头信息
+        List<SecurityScheme> result = new ArrayList<>();
+        ApiKey apiKey = new ApiKey("Authorization", "Authorization", "header");
+        result.add(apiKey);
+        return result;
+    }
+
+    private List<SecurityContext> securityContexts() {
+        //设置需要登录认证的路径
+        List<SecurityContext> result = new ArrayList<>();
+        result.add(getContextByPath());
+        return result;
+    }
+
+    private SecurityContext getContextByPath(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/brand/.*"))
                 .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        List<SecurityReference> result = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        result.add(new SecurityReference("Authorization", authorizationScopes));
+        return result;
     }
 
 }
